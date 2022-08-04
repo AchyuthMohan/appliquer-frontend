e6aa7bea845caca9ea125943730ceb5c84e637f8
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-08-04 23:31:11 +0530

    added basic structure

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import NavBar from '../../components/Navbar/NavBar'
+import Carousel from './Carousel'
+const Home = () => {
+  return (
+    <div>
+        <NavBar/>
+        <Carousel/>
+
+    </div>
+  )
+}
+
+export default Home
