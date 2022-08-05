f4773ccaae8a4258542eaf55f490789621073e1c
Achyuth Mohan <achyuthmohan1@gmail.com>
2022-08-05 21:55:47 +0530

    carousel init

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import NavBar from '../../components/NavBar/NavBar'
+import NavBar from '../../components/Navbar/NavBar'
 import Carousel from './Carousel'
 import Cards from '../../components/Cards/Cards'
 
